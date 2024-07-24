b8192dc4d3b063a4b22005bac07fdfd489e15dd7
deji2ghost <dejiwilliams9@gmail.com>
2024-07-24 22:51:39 +0200

    env

--- src/app/firebase/config.ts
+++ src/app/firebase/config.ts
@@ -1,4 +1,4 @@
-
+'use client'
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -13,17 +13,16 @@ import { getFirestore } from "firebase/firestore";
 // }
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAy0Im4FP9CyKqLJx8Sirt7DJmZLeyounM",
-  authDomain: "linksharingapp-67e8b.firebaseapp.com",
-  projectId: "linksharingapp-67e8b",
-  storageBucket: "linksharingapp-67e8b.appspot.com",
-  messagingSenderId: "145525860117",
-  appId: "1:145525860117:web:f7359d0cbe7a0862e4eb1c",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app)
-const db = getFirestore(app)
 
-export {app, auth, db}
+export {app, auth}
